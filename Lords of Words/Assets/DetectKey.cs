a0b7a32504b9c530e9b017b72ac0c7c3a8e379d7
andra-m <aandra.matei@gmail.com>
2016-12-15 22:32:33 +0200

    Updated DetectKey.cs

--- Lords of Words/Assets/DetectKey.cs
+++ Lords of Words/Assets/DetectKey.cs
@@ -5,31 +5,31 @@ using UnityEngine.UI;
 public class DetectKey : MonoBehaviour {
 
 	private KeyCode[] desiredKeys = {KeyCode.A, KeyCode.B, KeyCode.C, KeyCode.D, KeyCode.E, KeyCode.F, KeyCode.G, KeyCode.H, KeyCode.I, KeyCode.J, KeyCode.K, KeyCode.L, KeyCode.M, KeyCode.N, KeyCode.O, KeyCode.P, KeyCode.Q, KeyCode.R, KeyCode.S, KeyCode.T, KeyCode.U, KeyCode.V, KeyCode.W, KeyCode.X, KeyCode.Y, KeyCode.Z};
-
+	public KeyCode k;
 	// Use this for initialization
 	void Start (){
 	}
 
 	// Update is called once per frame
 	void Update (){
-		if (detectLetter ()){
-			keyPressed ();
+		k = Input.anyKey;
+		if (detectLetter (KeyCode k)){
+			keyPressed (KeyCode k);
 		}
 	}
 
 
 	public bool detectLetter (){
-		if (Input.anyKey) {
-			foreach (KeyCode i in desiredKeys) {
-				if (Input.GetKeyDown (i))
-					return true;
+		foreach (KeyCode i in desiredKeys) {
+			if (Input.GetKeyDown (i))
+				return true;
 			}
-			return false;
-		}
+		return false;
+		
 	}
 
 
-	public void keyPressed(){
+	public void keyPressed(KeyCode k){
 
 	}
 }
