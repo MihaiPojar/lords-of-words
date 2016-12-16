afc7bf81b2e1fcb73a987f2144d760282368e99e
Mihai-Corneliu Pojăr <mihaicorneliu@gmail.com>
2016-12-16 09:33:20 +0200

    Idee pentru getkeydown

--- Lords of Words/Assets/DetectKey.cs
+++ Lords of Words/Assets/DetectKey.cs
@@ -5,31 +5,37 @@ using UnityEngine.UI;
 public class DetectKey : MonoBehaviour {
 
 	private KeyCode[] desiredKeys = {KeyCode.A, KeyCode.B, KeyCode.C, KeyCode.D, KeyCode.E, KeyCode.F, KeyCode.G, KeyCode.H, KeyCode.I, KeyCode.J, KeyCode.K, KeyCode.L, KeyCode.M, KeyCode.N, KeyCode.O, KeyCode.P, KeyCode.Q, KeyCode.R, KeyCode.S, KeyCode.T, KeyCode.U, KeyCode.V, KeyCode.W, KeyCode.X, KeyCode.Y, KeyCode.Z};
-	public KeyCode k;
+	public string k;
 	// Use this for initialization
 	void Start (){
 	}
 
 	// Update is called once per frame
 	void Update (){
-		k = Input.anyKey;
-		if (detectLetter (KeyCode k)){
-			keyPressed (KeyCode k);
+		k = Input.inputString;
+			Debug.Log (k);
+			if (detectLetter (k)) {
+				keyPressed (k);
+			}
+
+		if (Input.GetKeyDown (KeyCode.A)) {
+			keyPressed ("a");
 		}
+		/*lse*/
 	}
 
 
-	public bool detectLetter (){
-		foreach (KeyCode i in desiredKeys) {
+	public bool detectLetter (string k){
+		/*foreach (KeyCode i in desiredKeys) {
 			if (Input.GetKeyDown (i))
 				return true;
 			}
+		return false;*/
 		return false;
-		
 	}
 
 
-	public void keyPressed(KeyCode k){
+	public void keyPressed(string k){
 
 	}
 }
