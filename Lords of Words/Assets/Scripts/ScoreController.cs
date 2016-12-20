38ea694e6a253db00533643abe9d653d6b7245d5
Zaharia Daniel <zaharia.daniel16@gmail.com>
2016-12-20 23:23:34 +0200

    script highscore

---
+++ Lords of Words/Assets/Scripts/ScoreController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreController : MonoBehaviour
+{
+
+	public string addScoreURL = "http://localhost:8080/highscores_post";
+	public string gethighscoreURL = "http://localhost:8080/highscores_get";
+
+	// Use this for initialization
+	void Start ()
+	{
+		StartCoroutine(GetScores());
+	}
+
+
+	IEnumerator PostScores(string name, int score)
+	{
+
+		string post_url = addScoreURL + "name=" + WWW.EscapeURL(name) + "&score=" + score;
+
+		WWW highscore_post = new WWW(post_url);
+		yield return highscore_post;
+
+		if (highscore_post.error != null)
+		{
+			print("There was an error updating the high score: " + highscore_post.error);
+		}
+	}
+
+
+	IEnumerator GetScores()
+	{
+		gameObject.guiText.text = "Loading Scores";
+		WWW highscore_get = new WWW(gethighscoreURL);
+		yield return highscore_get;
+
+		if (highscore_get.error != null)
+		{
+			print("There was an error getting the high score: " + highscore_get.error);
+		}
+		else
+		{
+			gameObject.guiText.text = highscore_get.text;
+		}
+	}
+
+
+}
+
