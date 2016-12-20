add6e2c3d867109670ffca3401db121570fc4f99
Lupancescu Diana <Lupancescu Diana>
2016-12-20 13:17:39 +0200

    Mesaj commit

---
+++ Lords of Words/Assets/Scripts/Menu.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+
+    public Canvas MainCanvas;
+    public Canvas HighScore;
+
+    void Awake()
+    {
+        HighScore.enabled = false;
+    }
+
+    public void HighscoreOn()
+    {
+        HighScore.enabled = true;
+        MainCanvas.enabled = false;
+    }
+
+    public void ReturnOn()
+    {
+        HighScore.enabled = false;
+        MainCanvas.enabled = true;
+    }
+
+    public void LoadOn()
+    {
+        Application.LoadLevel(1);
+    }
+
+    public void ExitOn()
+    {
+        Application.Quit();
+    }
+
+}
